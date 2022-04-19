dfafc50a1ce69e76126aee39acc19ae2131ae58d
msaad323 <saad500nbd@gmail.com>
2022-04-19 18:06:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-tjoukl
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-tjoukl)
---
+++ src/Navbar.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+  <nav>
+    <h2>Merka</h2>
+    <ul class="nav-list">
+      <li className= "nav-item">Home</li>
+      <li className= "nav-item" >Project</li>
+      <li className= "nav-item">Services</li>
+      <li className= "nav-item" >Blog</li>
+    </ul>
+  </nav>)
+
+          
+}
