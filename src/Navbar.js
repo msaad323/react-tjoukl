b6b79b1b5a02d386ca5a2ba071dbcab9da47be73
msaad323 <saad500nbd@gmail.com>
2022-04-19 19:31:40 +0530

    2 files modified

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,16 +1,15 @@
-import React from "react";
+import React from 'react';
 
 export default function Navbar() {
   return (
-  <nav>
-    <h2>Merka</h2>
-    <ul class="nav-list">
-      <li className= "nav-item">Home</li>
-      <li className= "nav-item" >Project</li>
-      <li className= "nav-item">Services</li>
-      <li className= "nav-item" >Blog</li>
-    </ul>
-  </nav>)
-
-          
+    <nav>
+      <h2>Merka</h2>
+      <ul className = "nav-list">
+        <li className="nav-item">Home</li>
+        <li className="nav-item">Project</li>
+        <li className="nav-item">Services</li>
+        <li className="nav-item">Blog</li>
+      </ul>
+    </nav>
+  );
 }
